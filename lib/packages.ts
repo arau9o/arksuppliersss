73d55c2306932c4d0e769e56e50006664fd8dd9f
arau9o <adamkhadrane2011@gmail.com>
2026-08-15 12:15:34 -0400

    Update packages.ts

--- lib/packages.ts
+++ lib/packages.ts
@@ -11,21 +11,21 @@ export const PACKAGES: RobuxPackage[] = [
   {
     id: "pkg_7000",
     robux: 7000,
-    price: 2599,
+    price: 3599,
     officialPrice: 6999,
     label: "7K ROBUX",
   },
   {
     id: "pkg_14000",
     robux: 14000,
-    price: 5000,
+    price: 7099,
     officialPrice: 12999,
     label: "14K ROBUX",
   },
   {
     id: "pkg_1700",
     robux: 21000,
-    price: 7500,
+    price: 9999,
     officialPrice: 19999,
     label: "21K ROBUX",
     popular: true,
@@ -33,7 +33,7 @@ export const PACKAGES: RobuxPackage[] = [
   {
     id: "pkg_28000",
     robux: 28000,
-    price: 10000,
+    price: 13999,
     officialPrice: 24999,
     label: "28K ROBUX",
   },
